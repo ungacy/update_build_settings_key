443bf0ea4add1d1340b25ecc748e938d851cce22
taoye <yetao@cienet.com.cn>
2016-12-26 16:05:47 +0800

    1.add example 2.update ‘ProvisioningStyle’ on when updating PROVISIONING_PROFILE_SPECIFIER

--- lib/fastlane/plugin/update_build_settings_key/actions/update_build_settings_key_action.rb
+++ lib/fastlane/plugin/update_build_settings_key/actions/update_build_settings_key_action.rb
@@ -6,16 +6,18 @@ module Fastlane
         require 'xcodeproj'
 
         info_plist_key = 'INFOPLIST_FILE'
-        identifier_key = params[:build_settings_key]
+        build_settings_key = params[:build_settings_key]
         project_path = params[:xcodeproj]
         configuration = params[:configuration]
         map = params[:map]
         project = Xcodeproj::Project.open(project_path)
-        pbxprojects = project.objects.select { |obj| obj.isa == 'PBXProject' }
-        # Set ProvisioningStyle to Manual
-        pbxprojects.each { |obj| obj.attributes.each { |attribute, value| value.each { |key, item| item['ProvisioningStyle'] = 'Manual' } if value.kind_of? Hash } }
+        # Set ProvisioningStyle to Manual if
+        if 'PROVISIONING_PROFILE_SPECIFIER' == build_settings_key
+          pbxprojects = project.objects.select { |obj| obj.isa == 'PBXProject' }
+          pbxprojects.each { |obj| obj.attributes.each { |attribute, value| value.each { |key, item| item['ProvisioningStyle'] = 'Manual' } if value.kind_of? Hash } }
+        end
         configs = project.objects.select { |obj| obj.isa == 'XCBuildConfiguration' && obj.to_s == configuration && obj.build_settings[info_plist_key] }
-        UI.user_error!("Info plist uses $(#{identifier_key}), but xcodeproj does not") unless configs.count > 0
+        UI.user_error!("Info plist uses $(#{build_settings_key}), but xcodeproj does not") unless configs.count > 0
         configs = configs.select do |obj|
           info_plist_value = obj.build_settings[info_plist_key]
           !map[info_plist_value].nil?
@@ -24,14 +26,14 @@ module Fastlane
         # For each of the build configurations, set specific profile
         configs.each do |c|
           info_plist_value = c.build_settings[info_plist_key]
-          c.build_settings[identifier_key] = map[info_plist_value]
+          c.build_settings[build_settings_key] = map[info_plist_value]
         end
         project.save
         UI.success("Successfully updated project ")
       end
 
       def self.description
-        "Updated code signing settings from 'Automatic' to a specific profile"
+        "Updated build settings key to a new value"
       end
 
       def self.authors
@@ -44,7 +46,7 @@ module Fastlane
 
       def self.details
         # Optional:
-        "Updated code signing settings from 'Automatic' to a specific profile"
+        "Updated build settings key to a new value including specific profile,\nwill Updated code signing settings from 'Automatic' to a specific profile when key is PROVISIONING_PROFILE_SPECIFIER"
       end
 
       def self.available_options
@@ -56,12 +58,28 @@ module Fastlane
                                          UI.user_error!("Path is invalid") unless File.exist?(value)
                                        end),
           FastlaneCore::ConfigItem.new(key: :configuration,
-                                       description: "name of your configuration"),
+                                       description: "name of your configuration ,such as 'Release'"),
           FastlaneCore::ConfigItem.new(key: :build_settings_key,
-                                       description: "key of your build setting"),
+                                       description: "key of the build setting you want update"),
           FastlaneCore::ConfigItem.new(key: :map,
                                        type: Hash,
-                                       description: "new value of your build setting in hash")
+                                       description: "KEY is path to Info.plist , VALUE is updated value of build setting key")
+        ]
+      end
+
+      def self.example_code
+        [
+          'update_build_settings_key(
+            xcodeproj: "Demo/Demo.xcodeproj",
+            configuration: "Release",
+            build_settings_key: "PROVISIONING_PROFILE_SPECIFIER",
+            map: {
+              "Demo Watch Extension/Info.plist" => "Demo WatchKit Extension 2",
+              "Demo Watch/Info.plist" => "Demo WatchKit App",
+              "Demo Today/Info.plist" => "Demo Today",
+              "Demo/Info.plist" => "Demo"
+            }
+          )'
         ]
       end
 
